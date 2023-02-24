85f2e3b80a81d2b3160fe2480b7888e154b30cc6
NanoB0tt <marianofassikoropecki@gmail.com>
2023-02-24 14:19:03 -0300

    added stores and slices

--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -1,94 +1,14 @@
-import { nanoid } from "nanoid";
-import { create } from "zustand";
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from '../stores/slices/todoSlice';
 
-export interface Todo {
-  id: string;
-  task: string;
-  done: boolean;
-}
-
-type State = {
-  todos: Todo[];
-  newTodo: string;
-}
-
-type Actions = {
-  addTodo: () => void;
-  addNewTodo: (task: string) => void;
-  checkDone: (id: string) => void;
-  updateTodo: (id: string, task: string) => void;
-  loadTodo: (todos: Todo[]) => void;
-  deleteTodo: (id: string) => void;
-}
-
-const addTodo = (todos: Todo[], task: string): Todo[] => [
-  ...todos,
-  {
-    id: nanoid(),
-    task,
-    done: false
+export const todoStore = configureStore({
+  reducer: {
+    todo: todoReducer,
   },
-];
-
-const checkDone = (todos: Todo[], id: string): Todo[] =>
-  todos.map(todo => ({
-    ...todo,
-    done: todo.id === id ? !todo.done : todo.done
-  }));
-
-const updateTodo = (todos: Todo[], id: string, task: string): Todo[] =>
-  todos.map(todo => ({
-    ...todo,
-    task: todo.id === id ? task : todo.task
-  }));
-
-const loadTodo = (todos: Todo[], fileTodo: Todo[]): Todo[] => [
-  ...todos,
-  ...fileTodo
-]
-
-const deleteTodo = (todos: Todo[], id: string): Todo[] =>
-  todos.filter(todo => todo.id !== id);
+});
 
 
-export const UseTodoStore = create<State & Actions>()((set) => ({
-  todos: [],
-  newTodo: "",
-  addTodo() {
-    set((state) => ({
-      todos: addTodo(state.todos, state.newTodo),
-      newTodo: ""
-    }));
-  },
-  addNewTodo(text: string) {
-    set(state => ({
-      ...state,
-      newTodo: text
-    }));
-  },
-  checkDone(id: string) {
-    set(state => ({
-      ...state,
-      todos: checkDone(state.todos, id)
-    }));
-  },
-  updateTodo(id: string, task: string) {
-    set(state => ({
-      ...state,
-      todos: updateTodo(state.todos, id, task)
-    }));
-  },
-  loadTodo(fileTodo: Todo[]) {
-    set(state => ({
-      ...state,
-      todos: loadTodo(state.todos, fileTodo)
-    }))
-  },
-  deleteTodo(id: string) {
-    set(state => ({
-      ...state,
-      todos: deleteTodo(state.todos, id)
-    }));
-  }
-}))
-
+// Infer the 'RootState' and 'AppDispatch' types from the store itself
+export type RootState = ReturnType<typeof todoStore.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof todoStore.dispatch;
---
+++ src/stores/slices/todoSlice.ts
@@ -0,0 +1,74 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+export interface Todo {
+  id: string;
+  task: string;
+  done: boolean;
+}
+
+export interface TodoState {
+  todos: Todo[];
+  newTodo: string;
+}
+
+const initialState: TodoState = {
+  todos: [],
+  newTodo: ""
+}
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state) => {
+      if (state.newTodo) {
+        state.todos = [
+          ...state.todos,
+          {
+            id: nanoid(),
+            task: state.newTodo,
+            done: false
+          }
+        ],
+          state.newTodo = ""
+      }
+    },
+    addNewTodo: (state, action: PayloadAction<string>) => {
+      state.newTodo = action.payload
+    },
+    checkDone: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.map(todo => ({
+        ...todo,
+        done: todo.id === action.payload ? !todo.done : todo.done
+      }))
+    },
+    updateTodo: {
+      reducer: (state, action: PayloadAction<Omit<Todo, "done">>) => {
+        state.todos = state.todos.map(todo => ({
+          ...todo,
+          task: todo.id === action.payload.id ? action.payload.task : todo.task
+        }))
+      },
+      prepare: (id: string, task: string) => {
+        return { payload: { id, task } }
+      }
+    },
+    loadTodo: (state, action: PayloadAction<Todo[]>) => {
+      state.todos = [
+        ...state.todos,
+        ...action.payload
+      ]
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload)
+    }
+  }
+});
+
+
+// Actions creators are generated for each case reducer function
+export const { addTodo, addNewTodo, checkDone, updateTodo, loadTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
