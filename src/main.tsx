feca644f573aacc2c99ce36f492142369d5edea4
NanoB0tt <marianofassikoropecki@gmail.com>
2023-02-24 16:07:34 -0300

    added easy-peasy provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { StoreProvider } from 'easy-peasy'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { todoStore } from './stores/TodoStore'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <StoreProvider store={todoStore}>
+        <App />
+      </StoreProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
