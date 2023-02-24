9d5b826f7fb43d394413b4f51dbbe74180bc20af
NanoB0tt <marianofassikoropecki@gmail.com>
2023-02-24 14:18:36 -0300

    added redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { ChakraProvider, theme } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
 import App from './App'
+import { todoStore } from './stores/TodoStore'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <Provider store={todoStore}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>,
 )
