5d310f06f9ef78f4cacda86a373fbde3308c7676
NanoB0tt <marianofassikoropecki@gmail.com>
2023-03-02 11:32:27 -0300

    resposive topBar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,15 +1,15 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import LoadTodo from "../LoadTodo/LoadTodo";
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
 
 const TopBar = () => {
 
   return (
-    <Flex justifyContent="flex-end" gap="4" mb="10">
+    <Box display={{ base: 'grid', sm: 'flex' }} gap="0.5rem">
       <Heading marginRight="auto">Todo List</Heading>
       <ThemeSwitcher />
       <LoadTodo />
-    </Flex>
+    </Box>
   )
 
 }
